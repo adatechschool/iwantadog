3bf337ff697ab479a05db2a1723e6f1a29be13fb
Agathe Begault <agathe.begault@gmail.com>
2019-12-02 16:54:21 +0100

    avancement

--- dog.py
+++ dog.py
@@ -1,5 +1,10 @@
 class Dog():
    def __init__(self):
       pass
-   print("Wouf! Wouf Wouf!")
-
+# definition attributs
+   def colour (self, couleur):
+      self.colour = couleur
+   def sex (self, sexe):
+      self.sex = sexe
+   def age (self, année, mois):
+      self.age = [année, mois]
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dog import *
 print("Va chercher!")
 def main():
    x = Dog()
+   x.colour("brun")
+   x.sex("male")
+   x.age(2, 4)
+   print(x.colour, x.sex, x.age)
 
 if __name__ == "__main__":
    main()
